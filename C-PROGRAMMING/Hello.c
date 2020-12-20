f724e0c56b581eb26da78cdad87af44b671ab73d
sourav sanyal <srvofficial1@gmail.com>
2020-12-20 17:19:39 +0530

    add all files

---
+++ C-PROGRAMMING/Hello.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main()
+{printf("\nhello world baby!");
+return 0;}
---
+++ C-PROGRAMMING/add.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+    {
+        int a,b,;
+        printf("Enter the two numbers:");
+        scanf("%d%d",&a,&b);
+        sum=a+b;
+        printf("Addition of two numbers=%d",sum);
+        retuen 0
+    }
+)
